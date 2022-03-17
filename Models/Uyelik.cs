54e2b3a79bea6f93cc55cf46e28307ee8070e666
serkansimsekk <101815742+serkansimsekk@users.noreply.github.com>
2022-03-17 18:57:02 +0300

    Add files via upload

---
+++ Models/Uyelik.cs
@@ -0,0 +1,12 @@
+public class Kayit{
+public string ad {get;set;}
+public string soyAd {get;set;}
+public bool ehliyetVarmi {get;set;}
+public decimal ortalama {get;set;}
+public int kacDilBiliyorsunuz {get;set;}
+
+public DateTime yas {get;set;}
+
+
+
+}
